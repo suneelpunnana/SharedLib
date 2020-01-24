ce15ab40fe454f7627f466764e2883cafbbaf068
MaheedharTalluri <58215374+MaheedharTalluri@users.noreply.github.com>
2020-01-24 18:23:25 +0530

    Update jenkinscollector.groovy

--- vars/jenkinscollector.groovy
+++ vars/jenkinscollector.groovy
@@ -1,5 +1,5 @@
 def call(body){
 
-sh 'curl -X GET http://http://3.14.126.190:8080/api/json?pretty=true --user admin:admin'
+sh 'curl -X GET http://3.14.126.190:8080/api/json?pretty=true --user admin:admin'
 
 }
