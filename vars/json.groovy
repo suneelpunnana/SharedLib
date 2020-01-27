95c9882f20c39418e76f5229da38d1c68649210d
Komaravelli Manikanta (Digital) <KO20094862@wipro.com>
2020-01-27 16:57:21 +0530

    DRY

--- vars/json.groovy
+++ vars/json.groovy
@@ -1,3 +1,9 @@
-def props = readJSON file: './sample.json '
-def jobname=props.ci.jobs.job.job_name
-println(jobname) 
+import groovy.json.*
+
+def jS = new JsonSlurper()
+ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${env.JOB_NAME}/sample.json"),"UTF-8"))
+def data = jS.parse(reader)  
+String j1=data.ci.jobs.job.job_name
+String j2=j1.replace("[","");
+String job=j2.replace("]","");
+println(job)
