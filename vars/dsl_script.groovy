879e247563d40277c1b77637e42581c165d94ceb
Komaravelli Manikanta (Digital) <KO20094862@wipro.com>
2020-01-27 13:05:17 +0530

    dsl added

---
+++ vars/dsl_script.groovy
@@ -0,0 +1,23 @@
+println(args[0])
+pipelineJob(args[0]) {
+
+
+definition{
+cpsScm {
+scm {
+        git {
+            remote {
+                name('origin')
+                url('https://github.com/maheedhar132/Devops_assesment.git')
+            }
+              
+        }
+    }
+  }
+triggers {
+        cron('* * * * *')
+    }
+
+
+}
+}
