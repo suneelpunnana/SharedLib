e922eeae1855658fec25c9aa67fdfd98f1ebaead
Komaravelli Manikanta (Digital) <KO20094862@wipro.com>
2020-01-28 13:24:36 +0530

    test groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,3 @@
 def call(){
-curl -s -XPOST 'http://3.134.110.122:8080/createItem?name=job' -u admin:1166851f05c42cf7f533e59eac3819e43d --data-binary @Test.xml -H "Content-Type:text/xml"
+curl -s -XPOST http://3.134.110.122:8080/createItem?name=job -u admin:1166851f05c42cf7f533e59eac3819e43d --data-binary @Test.xml -H Content-Type:text/xml
 }
