8a8af82a0bf61910fdf5398651859171ebf289bb
MaheedharTalluri <1210815148@gitam.in>
2020-01-29 19:11:48 +0530

    groovy file for running dsl script

--- vars/createjob.groovy
+++ vars/createjob.groovy
@@ -4,8 +4,8 @@ static url='http://18.217.178.163:8080/createItem?name='+jobname
 sh 'echo ${dsl}'
 sh 'curl -O https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/1.76/job-dsl-core-1.76-standalone.jar'
 
-sh 'java -jar job-dsl-core-1.76-standalone.jar ${dsl}'
-sh 'echo $jobname'
+sh "java -jar job-dsl-core-1.76-standalone.jar '${dsl}'"
+
 url='http://18.217.178.163:8080/createItem?name='+jobname
 sh "echo ${url}"
 sh "curl -s -XPOST  '$url' -u admin:119767fb81f22e2f10d8594e4201717e53 --data-binary @Test.xml -H Content-Type:text/xml"
