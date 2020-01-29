4e0d2d6c3e81b8144ba0f356cc29ee9341292487
MaheedharTalluri <1210815148@gitam.in>
2020-01-29 12:01:05 +0530

    groovy file for running dsl script

---
+++ vars/final.groovy
@@ -0,0 +1,22 @@
+import groovy.json.*
+
+def jS = new JsonSlurper()
+ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/sample.json"),"UTF-8"))
+def data = jS.parse(reader)  
+String j1=data.ci.jobs.job.job_name
+String j2=j1.replace("[","");
+String job=j2.replace("]","");
+
+String j=data.ci.jobs.job.dsl_fileName
+println(j)
+
+String j3=j.replace("[","");
+String dsl=j3.replace("]","");
+println(dsl)
+
+
+
+path="/var/lib/jenkins/workspace/${JOB_NAME}/"+dsl
+println(path)
+
+run(new File(path),[job] as String[])
