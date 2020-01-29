eb09887337cbc76fd3824e78200d52edbe9a6dac
MaheedharTalluri <1210815148@gitam.in>
2020-01-29 15:11:04 +0530

    groovy file for running dsl script

--- vars/createjob.groovy
+++ vars/createjob.groovy
@@ -1,5 +1,5 @@
-
-def call(){
-curl -X GET http://example.com/job/pipe/config.xml -u admin:1166851f05c42cf7f533e59eac3819e43d -o mylocalconfig.xml
-curl -s -XPOST 'http://3.134.110.122:8080/createItem?name="${jobname}"' -u admin:1166851f05c42cf7f533e59eac3819e43d --data-binary @mylocalconfig.xml -H "Content-Type:text/xml"
+def call(dsl,jobname){
+sh 'curl -O https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/1.76/job-dsl-core-1.76-standalone.jar'
+sh 'java -jar job-dsl-core-1.76-standalone.jar dsl'
+sh 'curl -s -XPOST http://3.134.110.122:8080/createItem?name=jobname -u admin:1166851f05c42cf7f533e59eac3819e43d --data-binary @${JOB_NAME}.xml -H Content-Type:text/xml'
 }
