481347ee7a32bd6f997e5da479e2a9f84f5482eb
MaheedharTalluri <1210815148@gitam.in>
2020-01-29 18:43:12 +0530

    groovy file for running dsl script

--- vars/createjob.sh
+++ vars/createjob.sh
@@ -1,3 +1,3 @@
 curl -O https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/1.76/job-dsl-core-1.76-standalone.jar
-java -jar job-dsl-core-1.76-standalone.jar $0
-curl -s -XPOST  $1 -u admin:119767fb81f22e2f10d8594e4201717e53 --data-binary @Test.xml -H Content-Type:text/xml
+java -jar job-dsl-core-1.76-standalone.jar dsltest.groovy
+curl -s -XPOST  http://18.217.178.163:8080/createItem?name=name -u admin:119767fb81f22e2f10d8594e4201717e53 --data-binary @Test.xml -H Content-Type:text/xml
