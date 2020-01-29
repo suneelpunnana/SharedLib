cb78e8f296cebd72769f329effce5dfeda0805a1
MaheedharTalluri <1210815148@gitam.in>
2020-01-29 14:42:29 +0530

    groovy file for running dsl script

--- vars/rungroovy.groovy
+++ vars/rungroovy.groovy
@@ -1,4 +1,5 @@
 def call(dsl){
+println(dsl)
 sh 'curl -O https://repo.jenkins-ci.org/public/org/jenkins-ci/plugins/job-dsl-core/1.76/job-dsl-core-1.76-standalone.jar'
 sh 'java -jar job-dsl-core-1.76-standalone.jar dsl'
 }
