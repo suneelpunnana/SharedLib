0a9d7cf17f3b582a6153947182789376f78dfab3
MaheedharTalluri <1210815148@gitam.in>
2020-01-30 17:31:47 +0530

    msg

--- vars/jtest.groovy
+++ vars/jtest.groovy
@@ -3,7 +3,7 @@ def data = readJSON file: "${env.WORKSPACE}/${json}"
 String j = data.ci.jobs.job.job_name
 String jenkins_jobname=j.replaceAll("\\[", "").replaceAll("\\]","");
 
-def d=data.ci.jobs.job.dsl_fileName
+String d=data.ci.jobs.job.dsl_fileName
 String dsl_fileName=d.replaceAll("\\[", "").replaceAll("\\]","");
 
 
