e935b23b34e0e78d81e38f94aba6017f76c0aaaa
MaheedharTalluri <1210815148@gitam.in>
2020-01-30 15:58:46 +0530

    msg

--- vars/jtest.groovy
+++ vars/jtest.groovy
@@ -5,6 +5,9 @@ def jS = new JsonSlurper()
  def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/sample.json"),"UTF-8"))
 def data = jS.parse(reader)
 j=data.ci.jobs.job.job_name
+def sharedData = new Binding()
+def shell = new GroovyShell(sharedData)
+sharedData.setProperty('j',j)
  
  
  
