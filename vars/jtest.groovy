6daf302765b97ceeb3a1aa893152510c9b9f3d73
MaheedharTalluri <1210815148@gitam.in>
2020-01-30 17:43:04 +0530

    msg

--- vars/jtest.groovy
+++ vars/jtest.groovy
@@ -4,7 +4,7 @@ String j = data.ci.jobs.job.job_name
 String jenkins_jobname=j.replaceAll("\\[", "").replaceAll("\\]","");
 println(jenkins_jobname)
 String d=data.ci.jobs.job.dsl_fileName
-String dsl_fileName=d.replaceAll("\\[", "").replaceAll("\\]","");
+String dsl_filename=d.replaceAll("\\[", "").replaceAll("\\]","");
 println(dsl_filename)
 
 }
