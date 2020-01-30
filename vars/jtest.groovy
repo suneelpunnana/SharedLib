4af67c2f6eb51e54296c2002c57c6aeeac0c1b9c
MaheedharTalluri <1210815148@gitam.in>
2020-01-30 15:55:34 +0530

    msg

--- vars/jtest.groovy
+++ vars/jtest.groovy
@@ -1,11 +1,15 @@
  
-def call(){
 import groovy.json.*
 
 def jS = new JsonSlurper()
  def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/sample.json"),"UTF-8"))
 def data = jS.parse(reader)
 j=data.ci.jobs.job.job_name
+ 
+ 
+ 
+ def call(){
+
 println(j)
 
 
