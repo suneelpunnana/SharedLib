4d4c9b5f126cbf09532400dfa1e77a3485548cf2
MaheedharTalluri <1210815148@gitam.in>
2020-01-31 15:57:23 +0530

    jsontest

--- vars/lastsuccessfulbuild.groovy
+++ vars/lastsuccessfulbuild.groovy
@@ -1,3 +1,3 @@
 def call(jobname){
-sh "curl -s http://18.217.178.163:8080/job/'${jobname}'/lastSuccessfulBuild/api/json --user admin:119767fb81f22e2f10d8594e4201717e53"
+sh "curl -s http://18.221.47.136:8080/job/'${jobname}'/lastSuccessfulBuild/api/json --user admin:119767fb81f22e2f10d8594e4201717e53"
 }
