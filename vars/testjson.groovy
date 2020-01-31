3203375f6435cd97210bec49cb6ca5cef7114148
MaheedharTalluri <1210815148@gitam.in>
2020-01-31 14:30:20 +0530

    jsontest

---
+++ vars/testjson.groovy
@@ -0,0 +1,10 @@
+def call(jsondata){
+def jsonString = ${jsondata}
+def jsonObj = readJSON text: jsonString
+String a=jsonObj.ci.jobs.job.job_name
+String jobname=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.ci.jobs.job.dsl_fileName
+String dslfilename=b.replaceAll("\\[", "").replaceAll("\\]","");
+env.DSL_NAME = dslfilename
+env.JENKINS_NAME = jobname
+}
